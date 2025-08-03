2fc96215801a6e4c8cea02ede51e5e947eac8017
Ashp116 <ashp116@outlook.com>
2025-08-03 01:23:55 -0400

    UPDATE: MemstoreManager singleton

--- src/memstore/MemstoreManager.ts
+++ src/memstore/MemstoreManager.ts
@@ -1,9 +1,19 @@
 import {MemStore} from "./Memstore";
 
 export class MemstoreManager {
+    private static instance: MemstoreManager;
     private stores = new Map<string, MemStore<any, any>>();
 
-    Get<K, V>(name: string): MemStore<K, V> {
+    private constructor() {}
+
+    public static getInstance(): MemstoreManager {
+        if (!MemstoreManager.instance) {
+            MemstoreManager.instance = new MemstoreManager();
+        }
+        return MemstoreManager.instance;
+    }
+
+    public Get<K, V>(name: string): MemStore<K, V> {
         if (!this.stores.has(name)) {
             this.stores.set(name, new MemStore<K, V>());
         }
